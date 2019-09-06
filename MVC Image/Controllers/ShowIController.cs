7f19ae0c139b38bbb67677f47fa2b0957443eb23
Gaurav Gajjar <Gajjarg135@outlook.com>
2019-09-06 19:01:24 +0530

    Add project files.

---
+++ MVC Image/Controllers/ShowIController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Image.Controllers
+{
+    public class ShowIController : Controller
+    {
+        // GET: ShowI
+        public ActionResult GetImageFromByteArray()
+        {
+            string imgPath = Server.MapPath("~/Content/Image/Gaurav.jpg");
+            byte[] byteData = System.IO.File.ReadAllBytes(imgPath);
+            string imgBase64Data = Convert.ToBase64String(byteData);
+            string imgDataURL = string.Format("data:image/jpg;base64,{0}", imgBase64Data);
+            ViewBag.ImageData = imgDataURL;
+            return View();
+        }
+    }
+}
